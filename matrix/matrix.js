36d31cbf9547153c2eabef44e23c34a8213edab6
MariaCamila <mariacmaza7@gmail.com>
2021-05-26 17:39:57 -0500

    Ejercicio de matrix segunda solucion

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,31 +1,22 @@
 
 export class Matrix {
-  constructor(matrix) {
-    this.matrix = matrix;
-  }
-
-  get columns() {
-    const columns = [];
-
-    for (let i = 0; i <= this.rows.length; i++) {
-      columns[i] = this.rows.map((row) => {
-        return row[i];
-      });
-    }
-    return columns;
+  constructor(str) {
+    this.rowMatrix = str.split('\n').map(row => row.split(' ').map(element => +element));
   }
 
   get rows() {
-    let rows = this.matrix;
+    return this.rowMatrix;
+  }
 
-    rows = rows.split('\n');
-    const resultado = rows.map((row) => {
-      const dividir_resultado = row.split(' ');
-
-      return dividir_resultado.map((number) => {
-        return Number(number);
-      });
-    });
-    return resultado;
+  get columns() {
+    let columnMatrix = [];
+    for (let i = 0; i < this.rowMatrix[0].length; i++) {
+      let tempArray = [];
+      for (let j = 0; j < this.rowMatrix.length; j++) {
+        tempArray.push(this.rowMatrix[j][i]);
+      }
+     columnMatrix.push(tempArray);
+    }
+    return columnMatrix;
   }
 }
